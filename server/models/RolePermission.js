7a9ed6151225da885602ea5e583bdd3183d66b57
Samy951 <samy.hamlat01@gmail.com>
2025-07-26 23:49:48 +0200

    Remove server files and configurations for the FLOTTEQ project, including models, controllers, routes, and middleware, as part of a complete project cleanup.

--- server/models/RolePermission.js
+++
@@ -1,9 +0,0 @@
-// server/models/RolePermission.js
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("RolePermission", {
-    roleId: DataTypes.INTEGER,
-    permissionId: DataTypes.INTEGER,
-  }, {
-    timestamps: false,
-  });
-};
--- server/models/UserRole.js
+++
@@ -1,9 +0,0 @@
-// server/models/UserRole.js
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("UserRole", {
-    userId: DataTypes.INTEGER,
-    roleId: DataTypes.INTEGER,
-  }, {
-    timestamps: false,
-  });
-};
